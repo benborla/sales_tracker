b433fe073671778b555a215416985fb517fcdd07
Ben Borla <ben.borla@lamudi.com>
2020-07-19 02:26:30 +0800

    removed admin

--- admin/src/components/ui/Layout/index.js
+++
@@ -1,7 +0,0 @@
-import * as React from 'react'
-import { Layout } from 'react-admin'
-import SalesTrackerAppBar from '../AppBar'
-
-const SalesTrackerLayout = props => <Layout {...props} appBar={SalesTrackerAppBar} />
-
-export default SalesTrackerLayout
--- admin/src/pages/dashboard/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Card, CardContent, CardHeader } from '@material-ui/core'
-
-export const Dashboard = () => (
-    <Card>
-        <CardHeader title='Sales Tracker' />
-        <CardContent>Welcome!</CardContent>
-    </Card>
-)
