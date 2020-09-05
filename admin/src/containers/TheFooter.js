2e7bcfc1b66b086cabc0cd5e26069198b72aa131
Ben Borla <ben.borla@lamudi.com>
2020-09-05 22:54:56 +0800

    updated footer

--- admin/src/containers/TheFooter.js
+++ admin/src/containers/TheFooter.js
@@ -5,10 +5,10 @@ const TheFooter = () => {
   return (
     <CFooter fixed={false}>
       <div>
-        <a href="https://coreui.io" target="_blank" rel="noopener noreferrer">CoreUI</a>
-        <span className="ml-1">&copy; 2020 creativeLabs.</span>
+        <a href="https://coreui.io" target="_blank" rel="noopener noreferrer">{process.env.REACT_APP_NAME}</a>
+        <span className="ml-1">&copy; 2020 Zeta.</span>
       </div>
-      <div className="mfs-auto">
+      <div className="mfs-auto" style={{ display: 'none' }}>
         <span className="mr-1">Powered by</span>
         <a href="https://coreui.io/react" target="_blank" rel="noopener noreferrer">CoreUI for React</a>
       </div>
